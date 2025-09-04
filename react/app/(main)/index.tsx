796c5902103d14db1c3368602f447b42708ab952
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2025-09-04 13:40:41 +0200

    [CHORE[: clean front view for the searchbar

--- react/app/(main)/index.tsx
+++ react/app/(main)/index.tsx
@@ -1,8 +1,9 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
 import { SearchBar } from '@/components/ui/searchbar';
 import { searchApi } from '@/services/search.ts'
 import { useAuth } from '@/contexts/authCtx';
 import { Button } from '@/components/ui/button'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Index() {
   const { signOut } = useAuth();
@@ -20,28 +21,59 @@ export default function Index() {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text
-        style={{margin: 12, fontWeight: 'bold'}}>
-        Search for a friend
-      </Text>
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.mainContainer}>
+          {/* Contenu centré */}
+          <View style={styles.centeredContent}>
+            <Text style={styles.titleText}>
+              Search for a friend
+            </Text>
+            <SearchBar
+              onSubmit={handleSearchSubmit}
+              onChangeText={handleSearchChange}
+            />
+          </View>
 
-      <SearchBar
-        onSubmit={handleSearchSubmit}
-        onChangeText={handleSearchChange}>
-      </SearchBar>
-
-      <Button
-        onPress={() => {
-          signOut();
-        }}
-        style={{margin: 48, fontSize: 18, fontWeight: 'bold'}}
-      >
-        <Text
-          style={{color: '#fff'}}
-    >
-          Sign Out</Text>
-      </Button>
-    </View>
-  );
+          <Button
+            onPress={() => {
+              signOut();
+            }}
+            style={styles.signOutButton}
+          >
+            <Text style={{color: '#fff'}}>
+              Sign Out
+            </Text>
+          </Button>
+        </View>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  safeArea: {
+    flex: 1
+  },
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'space-between' // Sépare le contenu du bouton
+  },
+  centeredContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleText: {
+    fontWeight: 'bold',
+    marginBottom: 20 // Espacement entre le titre et la barre de recherche
+  },
+  signOutButton: {
+    margin: 48,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+});
