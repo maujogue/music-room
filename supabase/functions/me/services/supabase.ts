2fe182c826a4a523ca0c890b01c33df2d2abc877
Leo Boulatrous <lboulatr@z1r4p4.42lyon.fr>
2025-09-10 11:03:38 +0200

    [CHORE] retrieve /me/services folder from #79

---
+++ supabase/functions/me/services/supabase.ts
@@ -0,0 +1,22 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+
+const base_url =
+  Deno.env.get('EXPO_PUBLIC_SUPABASE_URL') || 'http://localhost:54321';
+const supabase = createClient(
+  Deno.env.get('SUPABASE_URL')!,
+  Deno.env.get('SUPABASE_ANON_KEY')!
+);
+
+export async function getSupabaseEventByOwner(ownerId: string): Promise<any[]> {
+  const { data, error } = await supabase.from('events')
+	.select('*')
+	.eq('owner_id', ownerId);
+
+  if (error) {
+	console.error('Supabase error:', error);
+	throw new HTTPException(500, { message: `Error fetching events: ${error.message}` });
+  }
+
+  return data;
+}
