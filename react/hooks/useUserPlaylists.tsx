af24d2fdc5c2f101c8037899093e2586f57cfeb3
Loup Martineau <loumarti@student.42lyon.fr>
2025-10-13 15:17:02 +0200

    [ADD] Refetch after Sync playlist

--- react/hooks/useUserPlaylists.tsx
+++ react/hooks/useUserPlaylists.tsx
@@ -24,6 +24,7 @@ export function useUserPlaylists() {
   }, []);
 
   const refetch = useCallback(() => {
+    console.log('*********************** refetching playlists');
     fetchPlaylists();
   }, [fetchPlaylists]);
 
