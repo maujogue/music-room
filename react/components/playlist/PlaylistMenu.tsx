764a8d79b7b4d50e90dd5fffd596308274cbb953
Loup Martineau <loumarti@student.42lyon.fr>
2025-10-13 14:35:07 +0200

    [ADD] useAppToast generic Hook + AlertDialog Rfc

--- react/components/playlist/PlaylistMenu.tsx
+++ react/components/playlist/PlaylistMenu.tsx
@@ -1,16 +1,21 @@
 import { Menu, MenuItem, MenuItemLabel } from '@/components/ui/menu';
 import { Button, ButtonIcon } from '@/components/ui/button';
-import { MenuIcon } from '@/components/ui/icon';
+import { Icon, MenuIcon } from '@/components/ui/icon';
 import { syncSpotifyPlaylists } from '@/services/playlist';
+import { RefreshCw } from 'lucide-react-native';
+import { useAppToast } from '@/hooks/useAppToast';
+
+
 
 export default function PlaylistMenu() {
+  const toast = useAppToast();
+
   const handleSyncSpotifyPress = async () => {
     try {
       await syncSpotifyPlaylists();
-      alert('Playlists synced successfully!');
+      toast.show({ title: 'Synchronized with Spotify' });
     } catch (error) {
-      console.error('Error syncing playlists:', error);
-      alert('Failed to sync playlists.');
+      toast.error({ title: 'Failed to sync playlists' });
     }
   };
 
@@ -29,7 +34,11 @@ export default function PlaylistMenu() {
         textValue='spotify-sync'
         onPress={handleSyncSpotifyPress}
       >
-        <MenuItemLabel size='md'>Sync Spotify playlist</MenuItemLabel>
+        <Icon as={RefreshCw}
+          size='lg'
+          className='text-typography-600 mr-2'
+        />
+        <MenuItemLabel size='md'>Synchronize with Spotify</MenuItemLabel>
       </MenuItem>
     </Menu>
   );
