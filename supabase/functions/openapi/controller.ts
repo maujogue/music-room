b29f0bb6318488c974380b5ee06847c32f0adadc
Mathis Aujogue <maujogue15@gmail.com>
2026-01-19 14:43:17 +0100

    Revert "[UPD] openAPI -> update to cloud"
    This reverts commit c32d16d3229bbc35b70f5be6080c793c4ceac60c.

--- supabase/functions/openapi/controller.ts
+++ supabase/functions/openapi/controller.ts
@@ -7,34 +7,28 @@ export async function getOpenapiFile(c: Context): Promise<Response> {
   });
 }
 
-
 export async function getOpenapiFilesAsHtml(c: Context): Promise<Response> {
-  const origin = new URL(c.req.url).origin;
-
   const html = `<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Music App API Docs</title>
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
-  </head>
-  <body>
-    <div id="swagger-ui"></div>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <title>Music App API Docs</title>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
 
-    <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-    <script>
-      window.onload = () => {
-        window.ui = SwaggerUIBundle({
-          url: '${origin}/functions/v1/openapi/openapi.yml',
-          dom_id: '#swagger-ui',
-        });
-      };
-    </script>
-  </body>
-</html>`;
+      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+      <script>
+        window.onload = () => {
+          window.ui = SwaggerUIBundle({
+            url: 'http://localhost:54321/functions/v1/openapi/openapi.yml',
+            dom_id: '#swagger-ui',
+          });
+        };
+      </script>
+    </body>
+  </html>`;
 
-  return new Response(html, {
-    status: 200,
-    headers: { "content-type": "text/html; charset=utf-8" },
-  });
+  return c.html(html);
 }
